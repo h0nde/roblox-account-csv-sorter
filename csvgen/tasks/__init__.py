ae3e944be08fc99d6ced526c4a2e34dd345530e6
h0nda <41300123+davidbaszucki@users.noreply.github.com>
2020-12-08 09:09:01 +0100

    Update __init__.py

--- csvgen/tasks/__init__.py
+++ csvgen/tasks/__init__.py
@@ -2,10 +2,16 @@ from . import robux
 from . import premium
 from . import collectibles
 from . import settings
+from . import pin
+from . import groups
+from . import credit
 
 TASK_MAP = {
     "robux": robux.Task,
     "premium": premium.Task,
     "collectibles": collectibles.Task,
-    "settings": settings.Task
+    "settings": settings.Task,
+    "pin": pin.Task,
+    "groups": groups.Task,
+    "credit": credit.Task
 }
